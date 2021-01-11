390cd6916d94cab2a909e01111f6cf70da2e32fc
PiotrBorowski <piotrborowski97@op.pl>
2021-01-11 23:30:14 +0100

    małe zmiany

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from skmultiflow.data import LEDGenerator, ConceptDriftStream, LEDGeneratorDrift
 from skmultiflow.drift_detection import ADWIN, PageHinkley, KSWIN, DDM, EDDM
 from skmultiflow.lazy import KNNClassifier
-import numpy as np
 
 knn = KNNClassifier()
 
-driftStream = ConceptDriftStream()
+driftStream = ConceptDriftStream(width=500, position=6500)
 
 n_samples = 0
 corrects = 0
@@ -26,13 +25,12 @@ print(corrects, n_samples)
 # detectors
 adwin = ADWIN()
 ddm = DDM()
-kswin = KSWIN(alpha=0.005, window_size=300, stat_size=60,data=coldstartData)
+kswin = KSWIN(alpha=0.01)
 eddm = EDDM()
-#dobrac parametry do PH
 ph = PageHinkley()
 
 
-while n_samples < 8000:
+while n_samples < 10000:
     driftDataX, driftDataY = driftStream.next_sample()
     my_pred = knn.predict(driftDataX)
     correct = driftDataY[0] == my_pred[0]
@@ -40,7 +38,7 @@ while n_samples < 8000:
         corrects += 1
     n_samples += 1
 
-    adwin.add_element(corrects/n_samples)
+    adwin.add_element(0 if correct else 1)
     if adwin.detected_change():
         print('ADWIN', n_samples)
 
@@ -50,13 +48,13 @@ while n_samples < 8000:
     if ddm.detected_change():
         print('DDM', n_samples)
 
-    ph.add_element(corrects/n_samples)
+    ph.add_element(0 if correct else 1)
     if ph.detected_change():
         print('PH', n_samples)
 
-    kswin.add_element(corrects/n_samples)
-    if kswin.detected_change():
-        print('KSWIN', n_samples)
+    # kswin.add_element(corrects/n_samples)
+    # if kswin.detected_change():
+    #     print('KSWIN', n_samples)
 
     eddm.add_element(0 if correct else 1)
     if eddm.detected_change():
